ca5504eff9c6fad3bd9e9bc64b8e1d405e7bb405
Yuri Rider Sarah <yuri-rider-sarah@protonmail.com>
2020-12-02 23:50:39 +0100

    Allowed specifying output file

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ use llvm_sys::target_machine::*;
 
 fn main() {
     let src_file = std::env::args().nth(1).unwrap();
+    let dest_file = std::env::args().nth(2).unwrap_or(src_file.clone() + ".out");
     let mut chars: Vec<_> = std::fs::read_to_string(&src_file).unwrap().chars().collect();
     chars.reverse();
     let mut tokens = lexer::read_file_tokens(chars).unwrap();
@@ -67,7 +68,7 @@ fn main() {
         let program_main: &[u8] = std::include_bytes!("program_main.c");
         let main_path = temp_dir.path().clone().join(std::path::Path::new("main.c")).to_str().unwrap().to_string();
         std::fs::write(&main_path, program_main).unwrap();
-        let clang_exit_code = std::process::Command::new("clang").args(&[&obj_path[..], &main_path[..], "-static", "-o", "program"]).status().unwrap();
+        let clang_exit_code = std::process::Command::new("clang").args(&[&obj_path[..], &main_path[..], "-static", "-o", &dest_file[..]]).status().unwrap();
         if !clang_exit_code.success() {
             std::process::exit(1);
         }
