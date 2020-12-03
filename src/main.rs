aa32ba51f667baad415ea3cce9c489a3424487e4
Yuri Rider Sarah <yuri-rider-sarah@protonmail.com>
2020-12-03 14:49:34 +0100

    Added tests

--- src/main.rs
+++ src/main.rs
@@ -52,7 +52,11 @@ fn main() {
         let module = codegen::codegen_top(&exprs).unwrap();
         LLVMSetModuleDataLayout(module, LLVMCreateTargetDataLayout(target_machine));
         LLVMSetTarget(module, target_triple);
-        LLVMVerifyModule(module, LLVMVerifierFailureAction::LLVMAbortProcessAction, std::ptr::null_mut());
+        if LLVMVerifyModule(module, LLVMVerifierFailureAction::LLVMPrintMessageAction, std::ptr::null_mut()) != 0 {
+            eprintln!("Module:");
+            eprintln!("{}", std::ffi::CStr::from_ptr(LLVMPrintModuleToString(module)).to_string_lossy());
+            std::process::exit(1);
+        }
         let temp_dir = tempfile::tempdir().unwrap();
         let obj_path = temp_dir.path().clone().join(std::path::Path::new("object.o")).to_str().unwrap().to_string();
         let mut c_obj_path = CString::new(&obj_path[..]).unwrap().into_bytes_with_nul();
@@ -68,7 +72,10 @@ fn main() {
         let program_main: &[u8] = std::include_bytes!("program_main.c");
         let main_path = temp_dir.path().clone().join(std::path::Path::new("main.c")).to_str().unwrap().to_string();
         std::fs::write(&main_path, program_main).unwrap();
-        let clang_exit_code = std::process::Command::new("clang").args(&[&obj_path[..], &main_path[..], "-static", "-o", &dest_file[..]]).status().unwrap();
+        let clang_exit_code = std::process::Command::new("clang")
+            .args(&[&obj_path[..], &main_path[..], "-static", "-o", &dest_file[..]])
+            .status()
+            .unwrap();
         if !clang_exit_code.success() {
             std::process::exit(1);
         }
---
+++ tests/test.rs
@@ -0,0 +1,29 @@
+#[cfg(debug_assertions)]
+const COMPILER_PATH: &'static str = "target/debug/hagane";
+#[cfg(not(debug_assertions))]
+const COMPILER_PATH: &'static str = "target/release/hagane";
+
+macro_rules! test {
+    ( $name:ident , $output:expr ) => {
+        #[test]
+        fn $name() {
+            let compiler_out = std::process::Command::new(COMPILER_PATH)
+                .arg(concat!("tests/", stringify!($name), ".hgn"))
+                .stderr(std::process::Stdio::inherit())
+                .output()
+                .unwrap();
+            assert!(compiler_out.status.success());
+            let program_out = std::process::Command::new(concat!("tests/", stringify!($name), ".hgn.out"))
+                .stderr(std::process::Stdio::inherit())
+                .output()
+                .unwrap();
+            assert!(program_out.status.success());
+            assert_eq!(program_out.stdout, concat!($output, "\n").as_bytes());
+            std::fs::remove_file(concat!("tests/", stringify!($name), ".hgn.out")).unwrap();
+        }
+    };
+}
+
+test!(print, "1");
+test!(variable, "11");
+test!(function, "4");
